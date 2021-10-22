9d42910d287d7b416e433fdac86f57957d97bf0f
tmigot <tangi.migot@gmail.com>
2021-10-22 23:04:13 +0200

    up sha

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -20,7 +20,7 @@ using SolverBenchmark
 
 # NB: benchmarkpkg will run benchmarks/benchmarks.jl by default
 commit = benchmarkpkg(repo_name)  # current state of repository
-main = benchmarkpkg(repo_name, "c18749ffe2ff628a3f86943cd8d46b656a3b10a8")
+main = benchmarkpkg(repo_name, "b6bf3bbfd1776912400463323845dfe0baab9e57")
 judgement = judge(commit, main)
 
 commit_stats = bmark_results_to_dataframes(commit)
