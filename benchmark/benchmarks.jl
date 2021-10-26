b3d1e749988b962f6817ab79c79b07c082a5907a
tmigot <tangi.migot@gmail.com>
2021-10-26 02:28:06 +0200

    rmv Random

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,10 +1,8 @@
-using BenchmarkTools, DataFrames, Dates, DelimitedFiles, JLD2, Random
+using BenchmarkTools, DataFrames, Dates, DelimitedFiles, JLD2
 #JSO packages
 using CUTEst, NLPModels, NLPModelsKnitro, NLPModelsIpopt, SolverBenchmark, SolverCore
 #This package
 using DCISolver
-
-Random.seed!(1234)
 
 function runcutest(cutest_problems, solvers; today::String = string(today()))
   list = ""
